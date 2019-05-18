1b91f32f560bfc0239d6e0312234ec357558312d
Ansil <ansil@linxlabs.com>
2019-05-17 20:13:58 -0400

    Test auto build

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -81,7 +81,7 @@ function checkValidServiceWorker(swUrl) {
         response.status === 404 ||
         response.headers.get('content-type').indexOf('javascript') === -1
       ) {
-        // No service worker found. Probably a different app. Reload the page.
+        // No service worker found. Probably a different app. Reload the page..
         navigator.serviceWorker.ready.then(registration => {
           registration.unregister().then(() => {
             window.location.reload();
